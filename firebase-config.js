17d114efd8744b5d30c2fc5589eaaef3fc1e5c29
Bear Berlin <bearberlin@Bears-Mac-mini.localdomain>
2026-04-09 20:51:11 -0500

    Enable Firebase presence config

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 window.COLOR_CURRENT_FIREBASE_CONFIG = {
-  enabled: false,
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  enabled: true,
+  apiKey: "AIzaSyDn9zsZiTzDosmF8RyDqr3XZ0bm-erw5GM",
+  authDomain: "bear-72a69.firebaseapp.com",
+  databaseURL: "https://bear-72a69-default-rtdb.firebaseio.com/",
+  projectId: "bear-72a69",
+  storageBucket: "bear-72a69.firebasestorage.app",
+  messagingSenderId: "346094161855",
+  appId: "1:346094161855:web:7c0607917605f2906ddb0a",
+  measurementId: "G-JGCRDHMJQK"
 };
